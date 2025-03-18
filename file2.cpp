4cd926b24d669ba181e4d4d1ee58ff5122e800d5
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-03-18 16:05:25 +0700

    LoopingSwitchCase Do While

--- file2.cpp
+++ file2.cpp
@@ -16,8 +16,20 @@ int main(){
     while(x < 7){
         cout << "Nilai x : " << x << " Lari Keliling Lapangan" << endl;
         x = rand() % 10;
-        perulanganWhile++:
-    }while (x < 7);
+        perulanganWhile++;
+    }
+    cout << "Nilai x akhir : " << x << endl;
+    cout << "Jumlah perulangan : " << perulanganWhile << endl;
+    cout << endl;
+
+    //Perulangan Do While
+    cout << "Perulangan Do While" << endl;
+    do{
+        cout << "Nilai x : " << x << " Lari Keliling Lapangan" << endl;
+        x = rand() % 10;
+        perulanganDoWhile;
+    }
+    while (x < 7);
     cout << "Nilai x akhir : " << x << endl;
     cout << "Jumlah perulangan : " << perulanganWhile << endl;
  
