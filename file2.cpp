f61c6fbc3f1fea8ea7984fb68cfac025b1f39c4e
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-03-18 15:47:38 +0700

    LoopingSwitchCase Perulangan While

--- file2.cpp
+++ file2.cpp
@@ -5,4 +5,11 @@ using namespace std;
 
 int main(){
     //Perulangan While
+    cout << "Perulangan While" << endl;
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+    srand(time(0));
+    x = rand() % 10;
+    
 }
