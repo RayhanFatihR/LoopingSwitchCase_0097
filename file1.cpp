f49cc738751c266960b594b79db4c1dbf4fbb439
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-03-18 15:10:21 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Perulangan for untuk pencacahan naik
+    int i;
+    cout << "Perulangan For Pencacahan Naik" << endl;
+    for (i = 0; i <= 4; i++) {
+        cout << "Nilai i : " << i
+        << " Lari keliling lapangan" << endl;
+    }
+}
